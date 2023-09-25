56bb2c7f78fab3876c1c97699075a35b627b9407
DevExpressExampleBot <fakeEmail>
2023-09-25 16:32:54 +0400

    Source auto update [skip ci]

--- VB/SubDocumentIterator/Program.vb
+++ VB/SubDocumentIterator/Program.vb
@@ -1,23 +1,20 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
+Imports System
 Imports System.Windows.Forms
 Imports DevExpress.UserSkins
-Imports DevExpress.Skins
-Imports DevExpress.LookAndFeel
 
 Namespace SubDocumentIterator
-	Friend Module Program
-		''' <summary>
-		''' The main entry point for the application.
-		''' </summary>
-		<STAThread>
-		Sub Main()
-			Application.EnableVisualStyles()
-			Application.SetCompatibleTextRenderingDefault(False)
 
-			BonusSkins.Register()
-			Application.Run(New Form1())
-		End Sub
-	End Module
+    Friend Module Program
+
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread>
+        Sub Main()
+            Call Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            Call BonusSkins.Register()
+            Call Application.Run(New Form1())
+        End Sub
+    End Module
 End Namespace
